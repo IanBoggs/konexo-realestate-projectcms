a7f98ae0f7c175ec91d43bdaa0155ab5fcaaa652
IanBoggs <ian.r.boggs@googlemail.com>
2021-08-28 21:47:32 +0100

    added project

---
+++ TestMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
